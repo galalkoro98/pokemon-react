93d29b9b9dfd447f9cc87db1526a02aef4f5ceed
galalkoro98 <galal.koro@gmail.com>
2023-02-09 01:10:02 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "./styles/font.css";
+import "./styles/general.css";
+
 import { App } from "./components/App";
 
 const div = document.getElementById("root");
