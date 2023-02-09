d30e25577d635d0855fabf69f999b0b29a5dae0f
galalkoro98 <galal.koro@gmail.com>
2023-02-09 01:10:47 +0100

    modified

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
+import React from "react";
+import "./styles/App.css";
+import { Main } from "./Main";
 
 export const App = () => (
-    <>
+  <>
     <header>
-        <div className="header__content">
-            <h1>Pokemon</h1>
-        </div>
+      <div className="header__content">
+        <h1>Pokemon</h1>
+      </div>
     </header>
-    </>
-)
+    <Main />
+  </>
+);
