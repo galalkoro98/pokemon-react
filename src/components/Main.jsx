41eba529766689de53ef9c91c81fec5428a4d21f
galalkoro98 <galal.koro@gmail.com>
2023-02-09 01:14:13 +0100

    add main'

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { SectionPokemon } from "./SectionPokemon";
+import { SectionForm } from "./SectionForm";
+
+export const Main = () => (
+  <main>
+    <SectionPokemon />
+    <SectionForm />
+  </main>
+);
