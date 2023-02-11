24573e9efe660e8e6da5d20409fbef3006052f40
galalkoro98 <galal.koro@gmail.com>
2023-02-11 22:49:24 +0100

    add input function

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,17 @@
-export const Input = () => (
-  <input
-    className="search__field"
-    placeholder="search name or ID"
-    type="text"
-  />
-);
+import React from "react";
+
+import { usePokemon } from "../Pokemon";
+
+export const Input = () => {
+  const { inputField, handleInput, inputRef } = usePokemon();
+  return (
+    <input
+      className="search__field"
+      placeholder="search name or ID"
+      type="text"
+      value={inputField}
+      onChange={handleInput}
+      ref={inputRef}
+    />
+  );
+};
