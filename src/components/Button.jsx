bf2141dfec963f229d9ae4c7b22803a13210d490
galalkoro98 <galal.koro@gmail.com>
2023-02-11 22:48:49 +0100

    add a function to handle

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 import { Input } from "./Input";
+import { usePokemon } from "../Pokemon";
 
-export const Button = () => (
-  <div className="pokemon__button__container">
-    <Input />
-    <button className="pokemon__button">Search</button>
-  </div>
-);
+export const Button = () => {
+  const { handleClick } = usePokemon();
+  return (
+    <div className="pokemon__button__container">
+      <Input />
+      <button className="pokemon__button" type="submit" onClick={handleClick}>
+        Search
+      </button>
+    </div>
+  );
+};
